6d33e79c6c75cbc9170a68289f2f96a977ba5db5
helenacamanes <helenacamanes@gmail.com>
2026-07-06 22:14:23 -0300

    init: starting monorepo with basic web and mobile aplications

---
+++ packages/core-domain/src/index.ts
@@ -0,0 +1,10 @@
+// Exemplo de uma regra de negócio de Clean Architecture
+export interface Task {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
+export const getGuidedSteps = (taskTitle: string): string[] => {
+  return ["Passo 1: Ler com calma", `Passo 2: Iniciar a tarefa: ${taskTitle}`, "Passo 3: Confirmar conclusão"];
+};
